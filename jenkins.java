fc641b31838f612881ab00ba7673e6e881dc8c01
Pradeepyn-dev <147265126+Pradeepyn-dev@users.noreply.github.com>
2024-06-12 12:08:09 +0530

    Create jenkins.java

---
+++ jenkins.java
@@ -0,0 +1,8 @@
+// HelloWorld.java
+public class jenkins {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+    }
+}
